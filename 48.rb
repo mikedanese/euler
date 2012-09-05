cd7b7ec70574b8e5f735dd5600c3f2c3b319e252
Mike Danese <mikedanese@gmail.com>
2012-09-04 23:42:18 -0700

    multithreading it up

--- 48.rb
+++ 48.rb
@@ -1,32 +1,30 @@
 require 'thread'
-require 'debug'
 
-array =*(0..40)
+array =*(0..4)
 mutex = Mutex.new
 final_array = []
 thread_array=*(0..5)
 
 for i in 0..5
-   if !array.empty?
-      thread_array[i] = Thread.new do
+   mutex.synchronize do
+      number = array.pop
+   end
+   while !array.empty?
+      Thread.new do
          number = array.pop
          a = 1
          for i in 0..number do
             a = number * a
-            debugger
-            a = a.to_s[-10,10].to_i if a < 10000000000
+            a = a.to_s[-10,10].to_i if a > 10000000000
          end
          mutex.synchronize do            
             final_array << a
+            number = array.pop
          end
+         Thread.current.kill
       end
-   else
-      Thread.current.exit
    end
 end
 
-thread_array.each do |t|
-   t.join
-end
 
 p final_array
