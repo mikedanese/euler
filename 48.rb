cea9ab0a07a80c534a7549060927f58202236835
Mike Danese <mikedanese@gmail.com>
2012-09-05 01:03:52 -0700

    working with multithread. no apparent benifit to performance

--- 48.rb
+++ 48.rb
@@ -1,30 +1,32 @@
 require 'thread'
 
-array =*(0..4)
+array =*(1..1000)
 mutex = Mutex.new
 final_array = []
-thread_array=*(0..5)
+thread_array=*(0..3)
 
-for i in 0..5
-   mutex.synchronize do
-      number = array.pop
-   end
-   while !array.empty?
-      Thread.new do
-         number = array.pop
+thread_array.each do |i|
+   thread_array[i] = Thread.new do
+      until array.empty?
          a = 1
-         for i in 0..number do
-            a = number * a
+         n = 1
+         mutex.synchronize do
+            n = array.pop
+         end
+         for i in 1..n do
+            a = n * a
             a = a.to_s[-10,10].to_i if a > 10000000000
          end
          mutex.synchronize do            
             final_array << a
-            number = array.pop
          end
-         Thread.current.kill
       end
    end
 end
 
-
-p final_array
+main, *threads = Thread.list
+threads.each do |t|
+   t.join
+end
+sum = final_array.inject{|sum,x| sum + x }.to_s[-10,10].to_i
+p sum
