922cc6adf031f002f9afad4686963835c51ff452
Mike Danese <mikedanese@gmail.com>
2012-10-01 21:40:22 -0700

    fixed polynomial function

--- 27.rb
+++ 27.rb
@@ -6,7 +6,6 @@ num_of_threads = 4
 a = *(-1000..1000)
 b = a.product(a)
 
-b.each_slice(b.size()/(num_of_threads-1))
 
 def is_prime (n)
 	if n > 1
@@ -17,16 +16,15 @@ def is_prime (n)
 end
 
 def quadratic (n, b, c )
-	n**n + b*n + c
+	n**2 + b*n + c
 end
 
 final_array = []
-slice_size = b.size()/(num_of_threads-1)
+slice_size = b.size()/(num_of_threads)+1
 
 thread_slices = b.each_slice(slice_size).inject([]) do |memo, slice|
 	memo << slice
 end
-
 
 thread_slices.each do |slice|
 	Thread.new do
@@ -36,7 +34,7 @@ thread_slices.each do |slice|
 				n += 1
 			end
 			if memo[0] < n
-				memo = [n+1,var[0],var[1]]
+				memo = [n,var[0],var[1]]
 			end
 			memo
 		end
@@ -49,7 +47,5 @@ end
 
 main, *threads = Thread.list
 threads.each { |t| t.join}
-
-
 
 p final_array
